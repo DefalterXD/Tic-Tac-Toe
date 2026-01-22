ad08cd8a14a1a22e5f8f2eef227d6300187fe462
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-01-23 00:41:21 +0500

    feat(gameBoard): initialize board into vector

---
+++ script.js
@@ -0,0 +1,12 @@
+const gameBoard = (function gameBoardModuleControl() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let row = 0; row < rows; row++) {
+        board[row] = [];
+        for (let column = 0; column < columns; column++) {
+            // board[row].push(cell());
+        }
+    }
+})();
